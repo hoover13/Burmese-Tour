f02fd30b65d96568d7852114127b21a33bbf22eb
hoover13 <hoovermtk25@gmail.com>
2023-10-26 13:27:19 -0400

    Initial Commit

---
+++ Burma Guide/Burma_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Burma_GuideApp.swift
+//  Burma Guide
+//
+//  Created by Min Thu Khine on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Burma_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Burma Guide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Burma Guide
+//
+//  Created by Min Thu Khine on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
